3b22ba6b3c6baf72b11ff32c7df547d7585d36dc
FAstorgaR <francisco.astorga@usach.cl>
2025-06-21 22:42:03 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+API_KEY = "TU_API_KEY"
+API_SECRET = "TU_SECRET_KEY"
+BASE_URL = "https://paper-api.alpaca.markets"
+OBJETIVO_GANANCIA = 0.05  # +5%
+STOP_LOSS = -0.02         # -2%
+ARCHIVO_REGISTRO = "registro_operaciones.csv"
